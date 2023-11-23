8b2097eab4089639179830b1b2d86a204e68f31d
Melkysedeque Marques de Oliveira <100131595+Melkysedeque@users.noreply.github.com>
2023-11-23 20:44:18 -0300

    Add files via upload

---
+++ PHP/flob/aplicação/conteudo.php
@@ -0,0 +1,33 @@
+<div style="background-color: purple">
+   <center><h2>SERVIÇOS OFERECIDOS</h2><center>
+
+   </div>
+  <div>
+
+      </div>
+      <table>
+  <tr>
+    <th>Pedreiros</th>
+    <th>Cabeleiros</th>
+    <th>Barbeiro</th>
+    <th>Pintor</th>
+    <th>Confeiteira</th>
+  </tr>
+  <tr>
+    <td>Tefelone:</td>
+    <td>Telefone</td>
+    <td>Telefone</td>
+    <td>Telefone</td>
+    <td>Telefone</td>
+  </tr>
+  <tr>
+    <td>Faixa de Preço: por diára: entre 100 a 200</td>
+    <td>Faixa de preço:Depende do serviços</td>
+    <td>Faixa de Preço:entre 15 a 30 reais</td>
+    <td>Faixa de preço:por diaria: entre 50 a 200 reais</td>
+    <td>Faixa de preço:depende do tamanho e sabor do kit</td>
+  </tr>
+</table>
+
+
+</div>
